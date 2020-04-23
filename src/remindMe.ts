9d4a8a4b798730b6f5c4d10fbf300e39efc61f03
cg-cnu <sreenivas9alapati@gmail.com>
2020-04-23 17:33:50 +0930

    formatting fixes

--- src/remindMe.ts
+++ src/remindMe.ts
@@ -4,10 +4,10 @@ import * as sherlock from "sherlockjs";
 import * as datefns from "date-fns";
 
 const funTodos: string[] = [
-  "conquere the 🌍  tomorrow",
+  "conquere the 🌍 tomorrow",
   "git 🐙 commit 🐱 in 10 mins",
   "attend 🤝 meeting in 2 hours",
-  "💧 water💧 myself at 4 pm",
+  "💧 Hydrate 💧 myself at 4 pm",
   "🍊🍐 feed 🥕🥒 myself at 12:30 Am",
   "do 🎆 opensource 🎆 😎 after 5:30 pm",
 ];
@@ -29,14 +29,14 @@ export function activate(context: vscode.ExtensionContext) {
         const event = sherlock.parse(reminder);
         if (!event.eventTitle || !event.startDate) {
           vscode.window.showWarningMessage(
-            "🤖 Sorry boss! Couldnt understand, mind repeating ? 😉"
+            "🤖 Sorry boss! Couldnt understand, mind repeating ? 🤖"
           );
           return;
         }
         // reminder message
-        let reminderMessage: string = ` ⏰  ${
+        let reminderMessage: string = ` ⏰  '${
           event.eventTitle
-        } in ${datefns.distanceInWordsToNow(event.startDate)}`;
+        }' in ${datefns.distanceInWordsToNow(event.startDate)}`;
         vscode.window.showInformationMessage(reminderMessage);
 
         // reminder time
@@ -46,7 +46,7 @@ export function activate(context: vscode.ExtensionContext) {
         );
         var timer = setInterval(function () {
           vscode.window
-            .showInformationMessage(`⏰  ${event.eventTitle} now! ⏰`)
+            .showInformationMessage(`⏰  '${event.eventTitle}' now! ⏰`)
             .then(() => {
               clearTimeout(timer);
             });
